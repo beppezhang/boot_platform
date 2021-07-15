20a550e2bb2bb8d686e31778a025c770a0a10081
zhangsl <zhangsl2@yonghui.cn>
2021-07-15 20:17:12 +0800

    提交  更改

--- boot_platform/boot_test/src/main/java/com/beppe/git/User1.java
+++ boot_platform/boot_test/src/main/java/com/beppe/git/User1.java
@@ -6,6 +6,12 @@ public class User1 {
 
     private String name;
 
+    private String addr;
+
+    private int age;
+
+    private String job;
+
     public int getId() {
         return id;
     }
@@ -20,5 +26,29 @@ public class User1 {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
     }
 }
